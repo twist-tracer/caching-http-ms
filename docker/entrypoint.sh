80a2401d2cff1e2e498585017103e0347cdf4bff
Богданов Семен <twist.tracer@gmail.com>
2024-01-20 11:30:16 +0200

    <swagger> fixed entrypoint after master merge and install swagger deps

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,14 +1,19 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
 set -e
 
 yarn install
 
-if [ "$NODE_ENV" != 'production' ]
-then
-  exec ./node_modules/.bin/nodemon --exec node "$@";
-else
-  exec node "$@";
+# Run command with node if the first argument contains a "-" or is not a system command. The last
+# part inside the "{}" is a workaround for the following bug in ash/dash:
+# https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=874264
+if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ] || { [ -f "${1}" ] && ! [ -x "${1}" ]; }; then
+  if [ "$NODE_ENV" != 'production' ]
+  then
+    set -- ./node_modules/.bin/nodemon --exec node "$@";
+  else
+    set -- node "$@"
+  fi
 fi
 
-
+exec "$@";
